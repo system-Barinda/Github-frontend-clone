a4e3d5f117dfec55be40a2244d5a19b7bf49a3af
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-21 14:33:54 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import Footer from "./Pages/Footer";
 
 export default function App() {
   return (
-    <div className="flex flex-col h-screen bg-[#0d1117]">
+    <div className="flex flex-col h-full bg-[#0d1117] ">
 
       <Header />
 
@@ -14,9 +15,11 @@ export default function App() {
         {/* MAIN CONTENT */}
         <main className="flex-1 p-4 text-white">
           <MainContent />
+          <Footer />
         </main>
+        
       </div>
-
+     
     </div>
   );
 }
